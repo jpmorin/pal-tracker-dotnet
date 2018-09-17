5c856a549aa9394dc138ccde6d5641d97cb373f9
SebasFortin <sebastien.fortin@desjardins.com>
2018-09-17 14:23:23 -0600

    Add Swagger.

--- src/PalTracker/Startup.cs
+++ src/PalTracker/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
+using Swashbuckle.AspNetCore.Swagger;
+
 namespace PalTracker
 {
     public class Startup
@@ -37,6 +39,12 @@ namespace PalTracker
             ));
 
             services.AddSingleton<ITimeEntryRepository, InMemoryTimeEntryRepository>();
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,6 +54,16 @@ namespace PalTracker
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
 
             app.UseMvc();
         }
